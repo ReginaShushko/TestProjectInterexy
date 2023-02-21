9dbf7ef269c3e762ab16cbaf1845f60d037ce2ae
ReginaShushko <petrova.regina.mihajlovna@gamil.com>
2023-02-21 13:39:36 +0300

    added service-worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { store } from "./store/store";
-import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -16,3 +16,8 @@ root.render(
     </Provider>
   </BrowserRouter>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
